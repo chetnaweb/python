e38da9bd589245c9022f6ec3ca4a5814c5e3ec45
githubdemo <chetnaa.chetu@gmail.com>
2024-01-13 18:38:32 +0530

    simple code for phython

---
+++ python.py
@@ -0,0 +1,8 @@
+
+# Perform a calculation
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+sum_result = num1 + num2
+
+# Print the result
+print("The sum of {} and {} is: {}".format(num1, num2, sum_result))
